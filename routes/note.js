08e3d74b01e38940f800cb2ff57b1cb79cb9e0a8
Aleix Riba <aleix.riba@cabsa.es>
2020-03-29 14:00:30 +0200

    add endpoints crud

--- routes/note.js
+++ routes/note.js
@@ -2,16 +2,85 @@ import express from "express";
 import Note from "./../models/note";
 
 const router = express.Router();
+const ENTRYPOINT = "/note";
 
-router.post("/note", async (req, res) => {
+router.get(`${ENTRYPOINT}`, async (req, res) => {
+  try {
+    const notesDB = await Note.find();
+
+    res.json(notesDB);
+  } catch (error) {
+    return res.status(400).json({
+      message: "Internal error",
+      error
+    });
+  }
+});
+
+router.get(`${ENTRYPOINT}/:id`, async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const noteDB = await Note.findOne({ _id: id });
+
+    res.json(noteDB);
+  } catch (error) {
+    return res.status(400).json({
+      message: "Internal error",
+      error
+    });
+  }
+});
+
+router.post(`${ENTRYPOINT}`, async (req, res) => {
   const body = req.body;
 
   try {
     const noteDB = await Note.create(body);
 
-    res.status(200).json(noteDB);
+    res.json(noteDB);
   } catch (error) {
-    return res.status(500).json({
+    return res.status(400).json({
+      message: "Internal error",
+      error
+    });
+  }
+});
+
+router.delete(`${ENTRYPOINT}/:id`, async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const noteDB = await Note.findByIdAndDelete({ _id: id });
+
+    if (!noteDb) {
+      return res.status(400).json({
+        message: "That note does not exist",
+        error
+      });
+    }
+
+    res.json(noteDB);
+  } catch (error) {
+    return res.status(400).json({
+      message: "Internal error",
+      error
+    });
+  }
+});
+
+router.put(`${ENTRYPOINT}/:id`, async (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+
+  try {
+    const noteDB = await Note.findByIdAndUpdate({ _id: id }, body, {
+      new: true
+    });
+
+    res.json(noteDB);
+  } catch (error) {
+    return res.status(400).json({
       message: "Internal error",
       error
     });
